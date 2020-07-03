53fa7f5054aab57a9963c9300e2d6831e6ba154e
RAMYA RAJENDRA <ramyarajendra@RAMYAs-MacBook-Pro.local>
2020-07-03 17:19:28 -0500

    Updates

---
+++ precache-manifest.f57a38da7dc843766e1d0eada2fc0a82.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd5a5497791a7898c0913a6cc85e34bd",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "4a906b92f7518a839f54",
+    "url": "/portfolio/static/css/2.bd878acc.chunk.css"
+  },
+  {
+    "revision": "43cb17367076cc1581ef",
+    "url": "/portfolio/static/css/main.4b57565d.chunk.css"
+  },
+  {
+    "revision": "4a906b92f7518a839f54",
+    "url": "/portfolio/static/js/2.6639dd54.chunk.js"
+  },
+  {
+    "revision": "d8b4173f7a8b6c66c369ccd5944b6a3e",
+    "url": "/portfolio/static/js/2.6639dd54.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43cb17367076cc1581ef",
+    "url": "/portfolio/static/js/main.d9e84f29.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "80a0fb4baa1e99fe8ef42963b0fc4d31",
+    "url": "/portfolio/static/media/Ramya Rajendra.80a0fb4b.pdf"
+  },
+  {
+    "revision": "76e73036d4948d2f468510483c75699c",
+    "url": "/portfolio/static/media/Ramya.76e73036.jpeg"
+  },
+  {
+    "revision": "db90c97f41edb67d0fb60fbd0453ace9",
+    "url": "/portfolio/static/media/abc.db90c97f.jpg"
+  }
+]);
